6914ecdcb646f883e70ca92d8343c107ae95612a
sorooshaghaei <soroosh77aghaei@gmail.com>
2024-07-29 20:33:04 +0200

    creating navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>
-      <div className="text-center">
-      <h1>contact manager</h1>
-        <span className=" fa fa-user"> </span>
-      </div>
-
-      <div className="text-center">
-        <button className="btn btn-primary mx-3">hi click me</button>
-      </div>
+    <div className="App">
+      <Navbar />
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,24 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <div>Navbar</div>
+      <div className="navbar navbar-dark bg-dark">
+        <div className=" container">
+          <div className="row w-100">
+            <div className="col">
+              <i
+                className="m-2 fa fa-address-book fa-bounce"
+                style={{ color: "#B197FC" }}
+              ></i>
+              <span style={{ color: "#B197FC" }}>contact manager</span>
+            </div>
+            <div className="col">search</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 export default Navbar;
-
 
 // Export (export const Navbar):
 // Imported using exact name in curly braces: import { Navbar } from './Navbar';.
