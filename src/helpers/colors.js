85f066ab6958d4fe0ff19a70700e9fcd1fba014d
sorooshaghaei <soroosh77aghaei@gmail.com>
2024-08-09 17:36:08 +0200

    color palates

---
+++ src/helpers/colors.js
@@ -0,0 +1,7 @@
+export const LightBlue = "#8ECAE6";
+export const SkyBlue = "#219EBC";
+export const DarkBlue = "#023047";
+export const Yellow = "#FFB703";
+export const Orange = "#FB8500";
+export const Red = "#F94144";
+export const Green = "#90BE6D";
