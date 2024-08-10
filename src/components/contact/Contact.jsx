de2c8740d7db2e4347cae4f42f2a95920cd4016a
sorooshaghaei <soroosh77aghaei@gmail.com>
2024-08-10 21:26:35 +0200

    creating contact card component

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { DarkBlue, LightBlue, Orange } from "../../helpers/colors";
+
+const Contact = () => {
+  return (
+    <>
+      <div className="card col-12 col-md-6 " style={{ background: DarkBlue }}>
+        <div className="row g-0 p-3">
+          <div className="d-flex col-12 col-sm-4 col-md-4 justify-content-center justify-content-sm-start">
+            <img
+              src="https://via.placeholder.com/200"
+              className="img-fluid rounded-start p-3"
+              alt="contact pic"
+            />
+          </div>
+          <div className="col-12 col-sm-8 col-md-8 d-flex ">
+            <div className="card-body">
+              <ul className="list-group">
+                <li
+                  className="list-group-item"
+                  style={{ background: LightBlue }}
+                >
+                  Name:
+                </li>
+                <li
+                  className="list-group-item"
+                  style={{ background: LightBlue }}
+                >
+                  Email:
+                </li>
+                <li
+                  className="list-group-item"
+                  style={{ background: LightBlue }}
+                >
+                  Phone Number:
+                </li>
+                <li
+                  className="list-group-item"
+                  style={{ background: LightBlue }}
+                >
+                  Address:
+                </li>
+                <li
+                  className="list-group-item"
+                  style={{ background: LightBlue }}
+                >
+                  Relationship:
+                </li>
+              </ul>
+            </div>
+            <div className="d-flex flex-column my-4">
+              <button className="btn my-2 " style={{ background: Orange }}>
+                <i className="fa fa-trash"></i>
+              </button>
+              <button className="btn my-2" style={{ background: Orange }}>
+                <i className="fa fa-pencil"></i>
+              </button>
+              <button className="btn my-2" style={{ background: Orange }}>
+                <i className="fa fa-eye"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
--- src/components/contact/Contacts.jsx
+++ src/components/contact/Contacts.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { DarkBlue, LightBlue, Orange, SkyBlue } from "../../helpers/colors";
+import { SkyBlue } from "../../helpers/colors";
+import Contact from "./Contact";
 
 const Contacts = () => {
   return (
@@ -11,73 +12,7 @@ const Contacts = () => {
       </section>
 
       <section className="container">
-        <div className="card col-12 col-md-6 " style={{ background: DarkBlue }}>
-          <div className="row g-0 p-3">
-            <div className="d-flex col-12 col-sm-4 col-md-4 justify-content-center justify-content-sm-start">
-              <img
-                src="https://via.placeholder.com/200"
-                className="img-fluid rounded-start p-3"
-                alt="contact pic"
-              />
-            </div>
-            <div className="col-12 col-sm-8 col-md-8 d-flex ">
-              <div className="card-body">
-                <ul className="list-group">
-                  <li
-                    className="list-group-item"
-                    style={{ background: LightBlue }}
-                  >
-                    Name:
-                  </li>
-                  <li
-                    className="list-group-item"
-                    style={{ background: LightBlue }}
-                  >
-                    Email:
-                  </li>
-                  <li
-                    className="list-group-item"
-                    style={{ background: LightBlue }}
-                  >
-                    Phone Number:
-                  </li>
-                  <li
-                    className="list-group-item"
-                    style={{ background: LightBlue }}
-                  >
-                    Address:
-                  </li>
-                  <li
-                    className="list-group-item"
-                    style={{ background: LightBlue }}
-                  >
-                    Relationship:
-                  </li>
-                </ul>
-              </div>
-              <div className="d-flex flex-column my-4">
-                <button
-                  className="btn my-2 "
-                  style={{ background: Orange }}
-                >
-                  <i className="fa fa-trash"></i>
-                </button>
-                <button
-                  className="btn my-2"
-                  style={{ background: Orange }}
-                >
-                  <i className="fa fa-pencil"></i>
-                </button>
-                <button
-                  className="btn my-2"
-                  style={{ background: Orange }}
-                >
-                  <i className="fa fa-eye"></i>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Contact />
       </section>
     </>
   );
