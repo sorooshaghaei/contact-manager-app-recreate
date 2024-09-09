9b1b9a50668f6ec2032195838f29d5defe9c17cd
sorooshaghaei <soroosh77aghaei@gmail.com>
2024-09-09 18:45:30 +0200

    init json-server@0.17.0

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 import { Route, Routes, Navigate } from "react-router-dom";
@@ -16,6 +16,13 @@ const App = () => {
   const [getContact, setContact] = useState([]);
   // eslint-disable-next-line
   const [loading, setLoading] = useState(false); //for spinner
+
+
+  useEffect = () => {
+
+  }
+
+
   return (
     <>
       <div className="App">
